d423b6d64e3cdeaf8100d50e936469a823d01b53
Myles Pillay <MPillay@myless-mbp.home>
2021-11-24 16:56:32 +0000

    javascript started

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-let submit = document.querySelector(".submit");
 let container = document.getElementsByClassName("container");
-let inputText = document.querySelector("input");
+let submit = document.querySelector(".submit");
+let inputText = document.querySelector("input").innerHTML;
+const reset = document.getElementById("reset");
+
+let toDoList = document.querySelector(".list-Objects");
+
+
+submit.addEventListener('clicked'.map(inputText => {
+    return `<li>${inputText}</li>`;
+})
+toDoList.innerHTML += inputText.innerHTMl.join("");
+
